05ff4fa25c22817353127279cfbf72b79514dc4e
James O. Smith <james.smith@willowtreeapps.com>
2024-01-30 11:26:21 -0500

    Use List.Item from paper for PieceView

--- src/app/components/PieceView.tsx
+++ src/app/components/PieceView.tsx
@@ -1,10 +1,9 @@
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Text, View } from './Themed';
+import { View } from './Themed';
 import { Piece } from '../../api/dto/Piece';
 import React from 'react';
-import { Pressable, StyleSheet, useColorScheme } from 'react-native';
-import Colors from '../../constants/Colors';
+import { StyleSheet } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import { List } from 'react-native-paper';
 
 export interface PieceViewData {
   piece: Piece;
@@ -17,31 +16,19 @@ const handleClick = () => {
 export const PieceView = (props: PieceViewData) => {
   const theme = useTheme()
   const styles = StyleSheet.create({
-    pieceView: {
-      paddingHorizontal: 8,
-      paddingVertical: 4,
-    },
     container: {
       borderRadius: 8,
       overflow: 'hidden',
       backgroundColor: theme.colors.background
     },
-    title: {
-      fontSize: 15,
-    }
   })
 
   return (
     <View style={styles.container}>
-      <Pressable
-        onPress={handleClick}
-        style={styles.pieceView}
-        android_ripple={{
-          color: Colors[useColorScheme() ?? 'light'].selection,
-        }}>
-          <Text style={styles.title}>{props.piece.title}</Text>
-          <Text lightColor='rgba(64, 64, 64, 0.8)' darkColor='rgba(192, 192, 192, 0.8)'>{props.piece.composers.name}</Text>
-      </Pressable>
+      <List.Item
+        title={props.piece.title}
+        description={props.piece.composers.name}
+        onPress={handleClick} />
     </View>
   );
 };
