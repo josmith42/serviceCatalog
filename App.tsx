69f3ac5a90d87ffcddc8d3c3ac5c4970df8f3dc2
James O Smith <james.smith@willowtreeapps.com>
2024-02-06 14:08:26 -0500

    Fix rendering of status bar

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { useAppTheme } from './src/components/appTheme'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GlobalAppBar from './src/components/GlobalAppBar';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator()
 
@@ -14,6 +15,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PaperProvider theme={theme}>
+        <StatusBar />
         <NavigationContainer theme={theme}>
           <Stack.Navigator
               initialRouteName='Selections'
