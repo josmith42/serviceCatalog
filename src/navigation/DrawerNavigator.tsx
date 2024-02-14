8e93b516ed831229e51ad6bbfc389ed8fcc87a6e
James O Smith <james.smith@willowtreeapps.com>
2024-02-14 13:54:06 -0500

    Add icons to navigation drawer

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer"
 import DrawerNavAppBar from "../components/DrawerNavAppBar"
 import ServicesScreen from "../screens/ServicesScreen"
 import SelectionsScreen from "../screens/SelectionsScreen"
+import { Icon } from "react-native-paper"
 
 const Drawer = createDrawerNavigator()
 export function DrawerNavigator() {
@@ -12,11 +13,21 @@ export function DrawerNavigator() {
             <Drawer.Screen
                 name="ServicesScreen"
                 component={ServicesScreen}
-                options={{title: "Services"}}/>
+                options={{
+                    title: "Services",
+                    drawerIcon: ({color, size}) => (
+                       <Icon source="playlist-music" size={size} color={color} />
+                    )
+                }}/>
             <Drawer.Screen
                 name="SelectionsScreen"
                 component={SelectionsScreen}
-                options={{title: "Selections"}}/>
+                options={{
+                    title: "Selections",
+                    drawerIcon: ({color, size}) => (
+                       <Icon source="music" size={size} color={color} />
+                    )
+                }}/>
         </Drawer.Navigator>
     )
 }
